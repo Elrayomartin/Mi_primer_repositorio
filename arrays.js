81299cbd20c3b884b366e11f57b775b5f9a0dfcb
elrayomartin <kevin28022006@gmail.com>
2024-10-16 17:57:00 -0400

    hola

---
+++ arrays.js
@@ -0,0 +1,2 @@
+let arreglo= ['negro',32,'maiz','pera'] 
+console.log (arreglo [0])
---
+++ repaso.js
@@ -0,0 +1,11 @@
+function adivinaLaFruta(fruta) {
+   
+   if (fruta=='pera') {
+    return'adivinaste'
+}
+else {
+    return'prueba de nuevo'
+}
+}console.log (adivinaLaFruta('mora'))
+
+
